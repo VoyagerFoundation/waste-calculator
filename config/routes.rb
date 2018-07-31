29b116947cc5d0fa96f69867caa94c26551d71f2
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2018-07-31 23:40:28 +0100

    active admin + facts editor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
   get 'cheat_sheet', to: 'cheat_sheet#show'
   get '/', to: 'plastic_calculator#calculator'
   get 'hello_world', to: 'hello_world#index'
