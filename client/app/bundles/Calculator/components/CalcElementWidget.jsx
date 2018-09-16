500fdb40495a0f21888b53b730838ae46c47fd77
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2018-09-16 22:14:44 +0100

    Revert "report screen wip"
    This reverts commit 6d760c022a49c738310bde2d8a2981ae1122ce6f.

--- client/app/bundles/Calculator/components/CalcElementWidget.jsx
+++ client/app/bundles/Calculator/components/CalcElementWidget.jsx
@@ -42,7 +42,7 @@ export default class CalcElementWidget extends React.Component {
     return (
       <div key={item.id} id={item.id} className="row calc-item"> 
         <div className="col-sm-9">
-          <span className="font-weight-bold">{item.name}</span>
+          <span>{item.name}</span>
           {/* <i className="fas fa-info-circle info-tag"></i> */}
           <span className="badge badge-warning info-tag">{item.plastic_type}</span>
           {this.recyclable(item.recyclable)}
--- client/app/bundles/Calculator/components/CalculatorWidget.jsx
+++ client/app/bundles/Calculator/components/CalculatorWidget.jsx
@@ -1,56 +1,12 @@
 import React from 'react';
-import CalcElement from '../containers/CalcElement';
-import CalcResultsWidget from '../components/CalcResultsWidget';
+import CalcResultWidget from '../components/CalcResultWidget';
 import SelectGroupBtnWidget from '../components/SelectGroupBtnWidget';
+import CalcPadWidget from '../components/CalcPadWidget';
 
 export default class CalculatorWidget extends React.Component {
 
-  renderSectionElements(elements_obj, items){
-    return _.map(elements_obj, element => {
-      var item = items.find(function(e){ return e.id == element.id; });
-        var amount = 0;
-        if(item){
-          amount = item.amount;
-        }
-      return(<CalcElement item={element} key={element.id} amount={amount} />);
-    });
-  }
-
-  renderSections(elements_obj, items){
-    var sections = Object.keys(elements_obj);
-    return _.map(sections, section => {
-      return(
-        <div className="shadow pl-3 pl-3 pr-3 mb-4 mt-3">
-          <div className="row calc-section rounded-top">
-            <div className="col-md-12" key={section}>
-                <h4>{section}</h4>
-            </div>
-          </div>
-          {this.renderSectionElements(elements_obj[section], items)} 
-        </div>
-        );
-    });
-  }
-
-  renderGroups(elements_obj, items, groups_button){
-    var groups = Object.keys(elements_obj);
-    return _.map(groups, group => {
-      return(
-        <div key={group}>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="btn-group text-capitalize shadow">
-                {this.renderGroupButtons(groups_button)}
-              </div>
-            </div>
-            <div className="col-md-6 text-capitalize font-italic text-right">
-              <h4 className="pr-3">{group}</h4>
-            </div>
-          </div>
-          {this.renderSections(elements_obj[group],items)}
-        </div>
-        );
-    });
+  showReport(){
+    this.props.selectWasteGroup("report");
   }
 
   renderGroupButtons(groups){
@@ -59,9 +15,20 @@ export default class CalculatorWidget extends React.Component {
       return (<SelectGroupBtnWidget group={group.name} key={group.name} selectWasteGroup={selectWasteGroup} />);
     });
   }
+  
+  dispatchScrean(){
+    const { waste,  display_items, items , groups, selected_waste_group } = this.props;
+
+    if(selected_waste_group == "report"){
+      return(<CalcResultWidget />);
+    }
+    else {
+      return(<CalcPadWidget waste={waste} display_items={display_items} items={items} groups={groups} selected_waste_group={selected_waste_group} />);
+    }
+  }
 
   render() {
-    const { waste,  display_items, items , groups } = this.props;
+    const {  display_items , groups } = this.props;
   
     var categories = Object.keys(display_items);
     if(categories.size > 0){
@@ -78,20 +45,14 @@ export default class CalculatorWidget extends React.Component {
         </div>
         
         <div className="row">
-          <div className="col-md-8">
-            {this.renderGroups(display_items, items, groups)}
-          </div>
-          <div className="col-md-4">
-            <CalcResultsWidget waste={waste} />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-12 disclaimer">
-            * weight is average taken from amazon products and calculated from fluid ounces and shipping weight
+          <div className="col-md-12">
+            <div className="btn-group text-capitalize shadow">
+              {this.renderGroupButtons(groups)}
+            </div>
+            <button type="button" className="btn btn-primary shadow ml-2" onClick={this.showReport.bind(this)}>Report</button>
           </div>
         </div>
-
+        {this.dispatchScrean()}
       </div>
     );
   }
