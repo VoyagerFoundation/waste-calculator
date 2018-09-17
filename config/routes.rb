6a77f8bb944367f77d325487f0fb4dc30f7b9a47
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2018-09-17 21:57:38 +0100

    info screens ( contribution and calc alert )

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   get 'cheat_sheet', to: 'cheat_sheet#show'
+  get 'contribute', to: 'cheat_sheet#contribute'
   get '/', to: 'plastic_calculator#calculator'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
