b816eae8be0dd1293f47db4db14a22ecf93de59b
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2018-09-25 08:55:21 +0100

    repots screen

--- client/app/bundles/Calculator/components/CalcResultWidget.jsx
+++ client/app/bundles/Calculator/components/CalcResultWidget.jsx
@@ -2,8 +2,14 @@ import React from 'react';
  export default class CalcResultWidget extends React.Component {
   render(){
     return(
-      <div>
-        Report
+      <div className="container calculator">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="alert alert-dark" role="alert">
+              We are working hard to have report for you in place soon. Please stay tuned!
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
