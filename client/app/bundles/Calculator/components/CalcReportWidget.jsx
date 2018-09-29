eda24249c53b937822e10e24c94fdda5ef7a50a8
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2018-09-29 15:08:08 +0100

    travis update

--- client/app/bundles/Calculator/components/CalcReportWidget.jsx
+++ client/app/bundles/Calculator/components/CalcReportWidget.jsx
@@ -13,11 +13,18 @@ import CalcResultsWidget from './CalcResultsWidget';
             <div className="alert alert-dark" role="alert">
               We are working hard to have report for you in place soon. Please stay tuned!
             </div>
+
+            <li>How much % per type</li>
+            <li>How Much % each life area</li>
+            <li>How much recyclable</li>
+            <li>How much will be recyclable (?)</li>
+            <li>How Meany years it will dissolve</li>
+
           </div>    
           <div className="col-md-4">
             <CalcResultsWidget waste={waste} />
           </div>
-        </div>
+        </div>  
       </div>
     );
   }
