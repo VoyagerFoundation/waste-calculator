b5e56409272ef2a389c2c767fdc75f3c4dde6aee
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2019-07-03 15:45:12 +0100

    privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'cheat_sheet', to: 'cheat_sheet#show'
   get 'contribute', to: 'cheat_sheet#contribute'
   get 'about', to: 'cheat_sheet#about'
+  get 'privacy_policy', to: 'pages#privacy_policy'
   get '/', to: 'plastic_calculator#calculator'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
